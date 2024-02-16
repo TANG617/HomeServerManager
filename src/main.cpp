254f54f9b1e86ab39bc1badc4dfcbf161d724dcd
LiTang <litang0617@outlook.com>
2024-02-16 12:16:36 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,69 +1,110 @@
 #include <Arduino.h>
-#include <ESP8266Webhook.h>
 #include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
 
+#define controlPin 15
+#define statusPin 13
 
-#define _SSID "HOME"      // Your WiFi SSID
-#define _PASSWORD "T20030617"  // Your WiFi Password
-#define KEY "HELLO"        // Webhooks Key
-#define EVENT "HELLO Event"      // Webhooks Event Name
+const char *ssid = "HOME";
+const char *password = "T20030617";
 
-Webhook webhook(KEY, EVENT);    // Create an object.
+ESP8266WebServer server(80);
 
-void setup() {
-  Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  delay(1000);
+enum ServerStatus
+{
+	PowerOn = 0x00,
+	PowerOff = 0x01,
+	Unknown = 0x02,
+};
 
-  // Connect to WiFi
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to: ");
-  Serial.println(_SSID);
-  WiFi.begin(_SSID, _PASSWORD);
+ServerStatus serverStatus = Unknown;
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print("-");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi Connected");
-
-  // Print the IP address
-  Serial.print("IP Address: ");
-  Serial.print("http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("/");
-  digitalWrite(LED_BUILTIN, HIGH);
-
-//================================================================//
-//================================================================//
-
-  // Trigger with 3 values.
-  webhook.trigger("value1","value2","value3");
-  delay(5000);
-
-  // Trigger with 2 values.
-  webhook.trigger("ABC","XYZ");
-  delay(5000);
-
-  // Trigger with 1 value.
-  int Num = 1234;
-  webhook.trigger(String(Num));
-  delay(5000);
-
-  // Trigger without any value and get response.
-  int response = webhook.trigger();
-  if(response == 200)
-    Serial.println("OK");
-  else
-    Serial.println("Failed");
+void initStatus()
+{
+	serverStatus = (ServerStatus)digitalRead(statusPin);
 }
 
-void loop() {
-  // Nothing
+void handleRoot()
+{
+
+	server.send(200, "text/plain", "Hello from HomeServerManager");
+}
+
+void handlePowerOn()
+{
+	if (serverStatus == PowerOff)
+	{
+		digitalWrite(controlPin, 1);
+		server.send(200, "text/plain", "Power On");
+		delay(1000);
+		digitalWrite(controlPin,0);
+	}
+	else{
+		server.send(200, "text/plain", "Already Power On");
+	}
+}
+
+void handlePowerOff()
+{
+
+	server.send(200, "text/plain", "Power Off");
+}
+
+void handleNotFound()
+{
+
+	String message = "File Not Found\n\n";
+	message += "URI: ";
+	message += server.uri();
+	message += "\nMethod: ";
+	message += (server.method() == HTTP_GET) ? "GET" : "POST";
+	message += "\nArguments: ";
+	message += server.args();
+	message += "\n";
+	for (uint8_t i = 0; i < server.args(); i++)
+	{
+		message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+	}
+	server.send(404, "text/plain", message);
+}
+
+void setup(void)
+{
+	pinMode(controlPin, OUTPUT);
+	pinMode(statusPin, INPUT);
+
+	Serial.begin(115200);
+	WiFi.begin(ssid, password);
+	Serial.println("");
+
+	// Wait for connection
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		delay(500);
+		Serial.print(".");
+	}
+	Serial.println("");
+	Serial.print("Connected to ");
+	Serial.println(ssid);
+	Serial.print("IP address: ");
+	Serial.println(WiFi.localIP());
+
+	if (MDNS.begin("esp8266"))
+	{
+		Serial.println("MDNS responder started");
+	}
+
+	server.on("/", handleRoot);
+
+	server.onNotFound(handleNotFound);
+
+	server.begin();
+	Serial.println("HTTP server started");
+}
+
+void loop(void)
+{
+	server.handleClient();
 }
